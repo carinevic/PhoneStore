2a904895f9c277b892f107472091079ab2a4a954
Carine Ngwekwe <carine@Carines-MacBook-Pro.local>
2019-09-10 19:07:33 -0500

    functioning route

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import styled from 'styled-components';
 import logo from  '../logo.svg';
-import{ButtonContainer} from './Button';
+import {ButtonContainer} from './Button';
 
 
 
@@ -11,7 +11,7 @@ import{ButtonContainer} from './Button';
 export  default class Navbar extends Component {
     render() {
         return (
-           <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+           <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
 
 {/* 
 https://www.iconfinder.com/icons/1243689/call_phone_icon
@@ -42,8 +42,16 @@ https://www.iconfinder.com/Makoto_msk */
 </Link>
 
 
-           </nav>
+        
+           </NavWrapper>
         );
     }
 }
-
+const NavWrapper = styled.nav`
+background: var(--mainBlue);
+.nav-link{
+    color: var(--mainWhite)!important;
+    font-size: 1.3rem;
+    text-transform: capitalize;
+}
+`;
